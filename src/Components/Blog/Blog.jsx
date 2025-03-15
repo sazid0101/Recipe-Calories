15e8713718004bda28ec90eb55a18222c18d2083
sazid0101 <shahriarislamsazid@gmail.com>
2025-03-15 17:45:54 +0600

    card update

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import { BiAlarm } from "react-icons/bi";
+import { RiFireLine } from "react-icons/ri";
 
 const Blog = ({blog}) => {
-    const {recipe_img,recipe_id, recipe_name, short_description } = blog;
+    const {recipe_img,recipe_id, recipe_name, short_description,ingredient,preparing_time,calories } = blog;
     return (
-        <div className='w-100 border-2 p-5 m-5 rounded-2xl bg-slate-100 shadow-xl'>
+        <div className='w-100 border-2 p-5 m-5  rounded-2xl bg-slate-100 shadow-xl'>
             <img className='rounded-lg' src={recipe_img} alt="" />
-            <h1>{recipe_name}</h1>
+            <p>Recipe Id: {recipe_id}</p>
+            <h1 className='text-xl font-bold'>{recipe_name}.</h1>
             <p>{short_description}</p>
-            <div className="p-4 border-b border-gray-400"></div>
-            <h3>Ingredients:</h3>
+            <div className="p-2 border-b border-gray-400"></div>
+            <h3 className='font-bold'>Ingredients:{ingredient.length}</h3>
 
-            <button className="bg-teal-500 text-white rounded-full px-5 py-2 font-bold text-lg">Want to Cook</button>
+            {
+                ingredient.map(item => <ul><li>• {item}</li></ul>)
+            }
+            <div className="p-2 border-b border-gray-400"></div>
+
+            <div className='flex justify-between mt-1'>
+                <div className='flex items-center'>
+                    <BiAlarm></BiAlarm> 
+                    <p >{preparing_time} minutes</p>
+                </div>
+                <div className='flex items-center'>
+                    <RiFireLine></RiFireLine>
+                    <p>{calories} calories</p>
+                </div>
+            </div>
+            
+            <button className="bg-teal-500 text-white rounded-full px-5 py-2 font-bold text-lg mt-5">Want to Cook</button>
         </div>
     );
 };
